798233ae453595021c44a745c95f66c0b03865d7
SohailShaikh7860 <sohailshaikh6417@gmail.com>
2026-05-01 22:35:38 +0530

    fix: restructure Clerk auth routes to match configured sign-in/sign-up URLs

--- app/auth/sign-in/[[...sign-in]]/page.tsx
+++ app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return (
-    <div className="flex justify-center items-center min-h-[70vh]">
-      <SignIn forceRedirectUrl="/" />
+    <div className="flex justify-center items-center min-h-[calc(100vh-4rem)]">
+      <SignIn />
     </div>
   )
 }
